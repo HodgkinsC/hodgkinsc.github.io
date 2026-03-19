6b676e3bf1ac7a7fc849351266a5bb5e11910661
Callum <hodgkinsc@newlands.school.nz>
2026-03-19 16:58:17 +1300

    Page 47 plus school stuff

---
+++ school/navbar.js
@@ -0,0 +1,4 @@
+function set_navbar() {
+    document.getElementById("navbar").innerHTML = '<a href="index.html"><h3>Home page</h3></a><a href="map.html"><h3>Comic Map</h3></a><a href="resources.html"><h3>Art resources</h3></a>';
+    console.log("EXPLOSIONNN!!!!!")
+}
